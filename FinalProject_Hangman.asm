2edb1d3aee57f9fb8518a88cf8cb663574ff7eda
Scott <sbaroni2004@gmail.com>
2025-04-27 21:22:02 -0700

    Added macros for drawing diagonal lines and labels for drawing stick figure
    Added two macros for bitmap macros: drawDiagBackLine and drawDiagFrontLine. Also added labels for drawing the arms and a square head for stick figure in the main file.

--- FinalProject_Hangman.asm
+++ FinalProject_Hangman.asm
@@ -12,6 +12,11 @@
 .include "Bitmap_Macros.asm"
 .include "Strings.asm"
 
+# Bitmap Colors
+.eqv green, 0x0000FF00		# green color
+.eqv red, 0x00FF0000       	# red color
+.eqv white, 0xFFFFFFFF    	 # white color
+
 .data
 # strings in MARS are just an array of characters
 # even though this looks like one string, we will be able to access it character by character 
@@ -38,7 +43,6 @@ printUnderscoresLoop:
 getGuess:
 	readChar($t2)
 
-
 # Get Player 2's one letter guess
 #	- if they type 1 instead of a letter, jump to the code for guessing a word
 #	- if they guess a correct letter, replace appropriate underscore(s) with that letter
@@ -49,20 +53,46 @@ getGuess:
 #	- if they guess a correct word, print you win and exit
 #	- if they guess an incorrect word, return to prompting them for single letter guesses 
 
-# Bitmap stuff
 # To use the Bitmap Display Tool:
 # Go to Tools > Bitmap Display
 # Set width and height to 256, base address should be 0x10010000 (static data)
-# Select 'Connect to MIPS' then run the main file. Now the drawing should appear :]
+# Select 'Connect to MIPS' then run the main file. Now the drawing should appear
 launchDrawing:	# initial drawing when game is launched
-	drawHorizLine(100, 90, 150)	# draw top short line of gallow
-	drawHorizLine(50, 220, 150)	# draw bottom long line of gallow
-	drawHorizLine(50, 221, 150)	# (second line to make it thicker)
+	drawHorizLine(100, 90, 150, white)	# draw top short line of gallow
+	drawHorizLine(50, 220, 150, white)	# draw bottom long line of gallow
+	drawHorizLine(50, 221, 150, white)	# (second line to make it thicker)
 	
-	drawVertLine(100, 90, 220)	# draw long vertical line of gallow
-	drawVertLine(99, 90, 220)	# (second line to make it thicker)
-	drawVertLine(150, 90, 120)	# draw short vertical line of gallow
+	drawVertLine(100, 90, 220, white)	# draw long vertical line of gallow
+	drawVertLine(99, 90, 220, white)	# (second line to make it thicker)
+	drawVertLine(150, 90, 120, white)	# draw short vertical line of gallow
 
+# Drawing parts of body
+# Commented out lines are to make the drawing thicker if needed
+drawHead:
+	# (square head for now)
+	drawHorizLine(140, 140, 161, green)	# bottom of head
+	#drawHorizLine(140, 141, 161, green)
+	drawHorizLine(140, 120, 160, green)	# top of head
+	#drawHorizLine(140, 121, 160, green)
+	drawVertLine(140, 120, 140, green)	# left of head
+	#drawVertLine(141, 120, 140, green)
+	drawVertLine(160, 120, 141, green)	# right of head
+	#drawVertLine(161, 120, 141, green)
+drawBody:
+	drawVertLine(150, 140, 180, green)
+drawLeftArm:
+	drawDiagFrontLine(130, 159, 150, green)	# draw left arm
+	#drawDiagFrontLine(130, 160, 150, green)	# make left arm thicker
+drawRightArm:
+	drawDiagBackLine(150, 140, 160, green)	# draw right arm
+	#drawDiagBackLine(151, 140, 160, green)	# make right arm thicker
+drawLeftLeg:
+	drawDiagFrontLine(130, 199, 150, green)	# draw left leg
+	#drawDiagFrontLine(130, 200, 150, green)	# make left leg thicker
+drawRightLeg:
+	drawDiagBackLine(150, 180, 200, green)	# draw right leg
+	#drawDiagBackLine(151, 180, 200, green)	# make right leg thicker
+	
 # End of program
 exit:
 	li $v0, 10
